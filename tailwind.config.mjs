d41f6f6c71eebd1082831293bcd40c79cbed9215
Ismael Andres Parra Prato <iparra.sophosext@sistecredito.com>
2024-02-03 21:00:52 -0500

    add react and nextui

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
+const { nextui } = require("@nextui-org/react");
 export default {
   content: [
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
     "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
   ],
 
@@ -8,5 +10,5 @@ export default {
     extend: {},
   },
   darkMode: "class",
-  plugins: [],
+  plugins: [nextui()],
 };
